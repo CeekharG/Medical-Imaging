bb640546d0b093f7335691596f05fad052266d68
Shikhar Giri <42648701+CeekharG@users.noreply.github.com>
2020-07-09 14:42:12 -0500

    Add files via upload

---
+++ MRI Simulation/Code/MRI_SIMULATION/allcharts.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace MRI_SIMULATION
+{
+    partial class allcharts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(155, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(324, 14);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "ALL ANGIO AND PERFUSION VIRTUAL SIMULATION CHARTS";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(168, 49);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "MRI";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "BLOOD_VOLUME";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "BLOOD_FLOW";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "MEAN_TRANSIT_TIME";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "TIME_TO_PEAK";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 12;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(127, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ANGIO AND PERFUSION MRI VIRTUAL SIMULATION";
+            // 
+            // allcharts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 356);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Name = "allcharts";
+            this.Text = "All charts";
+            this.Load += new System.EventHandler(this.allcharts_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ MRI Simulation/Code/MRI_SIMULATION/allcharts.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
+using System;
+
+namespace MRI_SIMULATION
+{
+    public partial class allcharts : Form
+    {
+        OleDbConnection con;
+        OleDbCommand cmd;
+        OleDbDataAdapter adapter;
+        DataSet ds;
+        int rno = 0;
+        public allcharts()
+        {
+            InitializeComponent();
+        }
+
+        private void allcharts_Load(object sender, EventArgs e)
+        {
+            con = new OleDbConnection(@" provider=microsoft.jet.oledb.4.0; data source=C:\MRI_SIMULATION\MRI.mdb");
+
+            cmd = new OleDbCommand("Select * from MRIRECORDS", con);
+
+            OleDbDataReader mydatareader;
+
+
+
+            con.Open();
+
+            mydatareader = cmd.ExecuteReader();
+
+            while (mydatareader.Read())
+            {
+
+                this.chart1.Series["MRI"].Points.AddXY(mydatareader.GetString(6), mydatareader.GetString(1));
+                this.chart1.Series["BLOOD_VOLUME"].Points.AddXY(mydatareader.GetString(6), mydatareader.GetString(2));
+                this.chart1.Series["BLOOD_FLOW"].Points.AddXY(mydatareader.GetString(6), mydatareader.GetString(3));
+                this.chart1.Series["MEAN_TRANSIT_TIME"].Points.AddXY(mydatareader.GetString(6), mydatareader.GetString(4));
+                this.chart1.Series["TIME_TO_PEAK"].Points.AddXY(mydatareader.GetString(6), mydatareader.GetString(5));
+            }
+
+        }
+    }
+}
